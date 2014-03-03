53e3537ce03b0bdacb73f824faaca92f752b9199
Jack <jackwu95@gmail.com>
2014-03-02 22:45:36 -0500

    Some great cleanups

--- SpookCam-Pixel/SpookCam/SpookCam/ImageProcessor.m
+++ SpookCam-Pixel/SpookCam/SpookCam/ImageProcessor.m
@@ -8,103 +8,103 @@
 
 #import "ImageProcessor.h"
 
-@interface ImageProcessor () {
-    
-}
+@interface ImageProcessor ()
 
 @end
 
 @implementation ImageProcessor
 
 + (instancetype)sharedProcessor {
-    static id sharedInstance = nil;
-    
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        sharedInstance = [[self alloc] init];
-    });
-    
-    return sharedInstance;
+  static id sharedInstance = nil;
+  
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    sharedInstance = [[self alloc] init];
+  });
+  
+  return sharedInstance;
 }
 
 #pragma mark - Public
 
 - (void)processImage:(UIImage*)inputImage {
-    UIImage * outputImage = [self processUsingPixels:inputImage];
-    
-    if ([self.delegate respondsToSelector:@selector(imageProcessorFinishedProcessingWithImage:)]) {
-        [self.delegate imageProcessorFinishedProcessingWithImage:outputImage];
-    }
+  UIImage * outputImage = [self processUsingPixels:inputImage];
+  
+  if ([self.delegate respondsToSelector:@selector(imageProcessorFinishedProcessingWithImage:)]) {
+    [self.delegate imageProcessorFinishedProcessingWithImage:outputImage];
+  }
 }
 
 #pragma mark - Private
 
 #define Mask8(x) ( (x) & 0xFF )
 #define R(x) ( Mask8(x) )
-#define G(x) ( Mask8((x) >> 8)  )
-#define B(x) ( Mask8((x) >> 16) )
-#define A(x) ( Mask8((x) >> 24) )
+#define G(x) ( Mask8(x >> 8 ) )
+#define B(x) ( Mask8(x >> 16) )
+#define A(x) ( Mask8(x >> 24) )
 #define RGBAMake(r, g, b, a) ( Mask8(r) | Mask8(g) << 8 | Mask8(b) << 16 | Mask8(a) << 24 )
 - (UIImage *)processUsingPixels:(UIImage*)inputImage {
-    
-    // 1. Get the raw pixels of the image
-    UInt32 * pixels;
-    
-    CGImageRef inputCGImage = [inputImage CGImage];
-    NSUInteger width = CGImageGetWidth(inputCGImage);
-    NSUInteger height = CGImageGetHeight(inputCGImage);
-    
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    
-    NSUInteger bytesPerPixel = 4;
-    NSUInteger bytesPerRow = bytesPerPixel * width;
-    NSUInteger bitsPerComponent = 8;
-    
-    pixels = (UInt32 *) calloc(height * width, sizeof(UInt32));
-    
-    CGContextRef context = CGBitmapContextCreate(pixels, width, height,
-                                                 bitsPerComponent, bytesPerRow, colorSpace,
-                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
-    
-    CGContextDrawImage(context, CGRectMake(0, 0, width, height), inputCGImage);
-    
-    // 2. Do some processing!
-    UInt32 * currentPixel = pixels;
-    for (NSUInteger j = 0; j < height; j++) {
-        for (NSUInteger i = 0; i < width; i++) {
-            UInt32 color = *currentPixel;
-            
-            // Average of RGB = greyscale
-            UInt32 averageColor = (R(color) + G(color) + B(color)) / 3.0;
-            
-            // Add some random graininess (noise)
-            int magnitude = 80;
-            int noise = (arc4random() % magnitude) - magnitude/2;
-            averageColor += noise;
-            
-            //Clamp
-            averageColor = MAX(0, MIN(255, averageColor));
-            
-            *currentPixel = RGBAMake(averageColor, averageColor, averageColor, A(color));
-            currentPixel++;
-        }
+  
+  // 1. Get the raw pixels of the image
+  UInt32 * pixels;
+  
+  CGImageRef inputCGImage = [inputImage CGImage];
+  NSUInteger width = CGImageGetWidth(inputCGImage);
+  NSUInteger height = CGImageGetHeight(inputCGImage);
+  
+  CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+  
+  NSUInteger bytesPerPixel = 4;
+  NSUInteger bytesPerRow = bytesPerPixel * width;
+  NSUInteger bitsPerComponent = 8;
+  
+  pixels = (UInt32 *) calloc(height * width, sizeof(UInt32));
+  
+  CGContextRef context = CGBitmapContextCreate(pixels, width, height,
+                                               bitsPerComponent, bytesPerRow, colorSpace,
+                                               kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+  
+  CGContextDrawImage(context, CGRectMake(0, 0, width, height), inputCGImage);
+  
+  // 2. Do some processing!
+  UInt32 * currentPixel = pixels;
+  for (NSUInteger j = 0; j < height; j++) {
+    for (NSUInteger i = 0; i < width; i++) {
+      UInt32 color = *currentPixel;
+      
+      // Average of RGB = greyscale
+      UInt32 averageColor = (R(color) + G(color) + B(color)) / 3.0;
+      
+      // Add some random graininess (noise)
+      int magnitude = 80;
+      int noise = (arc4random() % magnitude) - magnitude/2;
+      averageColor += noise;
+      
+      //Clamp
+      averageColor = MAX(0, MIN(255, averageColor));
+      
+      *currentPixel = RGBAMake(averageColor, averageColor, averageColor, A(color));
+      currentPixel++;
     }
-    
-    // 3. Create a new UIImage
-    CGImageRef newCGImage = CGBitmapContextCreateImage(context);
-    UIImage * processedImage = [UIImage imageWithCGImage:newCGImage];
-    
-    // 4. Cleanup!
-    CGColorSpaceRelease(colorSpace);
-    CGContextRelease(context);
-    free(pixels);
-    
-    return processedImage;
+  }
+  
+  // 3. Create a new UIImage
+  CGImageRef newCGImage = CGBitmapContextCreateImage(context);
+  UIImage * processedImage = [UIImage imageWithCGImage:newCGImage];
+  
+  // 4. Cleanup!
+  CGColorSpaceRelease(colorSpace);
+  CGContextRelease(context);
+  free(pixels);
+  
+  return processedImage;
 }
+#undef RGBAMake
 #undef R
 #undef G
 #undef B
 #undef A
+#undef Mask8
 
 #pragma mark Helpers
 
--- SpookCam-Pixel/SpookCam/SpookCam/ViewController.m
+++ SpookCam-Pixel/SpookCam/SpookCam/ViewController.m
@@ -9,14 +9,11 @@
 #import "ViewController.h"
 #import "ImageProcessor.h"
 
-@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPopoverControllerDelegate, ImageProcessorDelegate>
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, ImageProcessorDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *mainImageView;
 
-//For presenting imagePicker on iPad
-@property (strong, nonatomic) UIPopoverController * imagePickerPopoverController;
 @property (strong, nonatomic) UIImagePickerController * imagePickerController;
-
 @property (strong, nonatomic) UIImage * workingImage;
 
 @end
@@ -27,67 +24,48 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+  [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 #pragma mark - Custom Accessors
 
 - (UIImagePickerController *)imagePickerController {
-    if (!_imagePickerController) { /* Lazy Loading */
-        _imagePickerController = [[UIImagePickerController alloc] init];
-        _imagePickerController.allowsEditing = NO;
-        _imagePickerController.delegate = self;
-    }
-    return _imagePickerController;
+  if (!_imagePickerController) { /* Lazy Loading */
+    _imagePickerController = [[UIImagePickerController alloc] init];
+    _imagePickerController.allowsEditing = NO;
+    _imagePickerController.delegate = self;
+  }
+  return _imagePickerController;
 }
 
 #pragma mark - IBActions
 
 - (IBAction)takePhotoFromCamera:(UIBarButtonItem *)sender {
-    if (self.imagePickerPopoverController) {
-        //popover is still showing
-        return;
-    }
-    self.imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
-    [self presentViewController:self.imagePickerController animated:YES completion:nil];
+  self.imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+  [self presentViewController:self.imagePickerController animated:YES completion:nil];
 }
 
 - (IBAction)takePhotoFromAlbum:(UIBarButtonItem *)sender {
-    self.imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-        if (self.imagePickerPopoverController) {
-            //popover is still showing
-            return;
-        }
-        self.imagePickerPopoverController = [[UIPopoverController alloc]
-                                             initWithContentViewController:self.imagePickerController];
-        self.imagePickerPopoverController.delegate = self;
-        [self.imagePickerPopoverController presentPopoverFromBarButtonItem:sender
-                                                  permittedArrowDirections:UIPopoverArrowDirectionAny
-                                                                  animated:YES];
-    }
-    else {
-        [self presentViewController:self.imagePickerController animated:YES completion:nil];
-    }
+  self.imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+  [self presentViewController:self.imagePickerController animated:YES completion:nil];
 }
 
 - (IBAction)savePhoto:(UIBarButtonItem *)sender {
-    if (!self.workingImage) {
-        return;
-    }
-    UIImageWriteToSavedPhotosAlbum(self.workingImage, nil, nil, nil);
+  if (!self.workingImage) {
+    return;
+  }
+  UIImageWriteToSavedPhotosAlbum(self.workingImage, nil, nil, nil);
 }
 
 #pragma mark - Private
 
 - (void)setupWithImage:(UIImage*)image {
-    self.workingImage = image;
-
-    // Commence with processing!
-    [ImageProcessor sharedProcessor].delegate = self;
-    [[ImageProcessor sharedProcessor] processImage:image];
+  self.workingImage = image;
+  
+  // Commence with processing!
+  [ImageProcessor sharedProcessor].delegate = self;
+  [[ImageProcessor sharedProcessor] processImage:image];
 }
 
 #pragma mark - Protocol Conformance
@@ -95,40 +73,19 @@
 #pragma mark - ImageProcessorDelegate
 
 - (void)imageProcessorFinishedProcessingWithImage:(UIImage *)outputImage {
-    self.mainImageView.image = outputImage;
-    self.mainImageView.backgroundColor = [UIColor redColor];
+  self.mainImageView.image = outputImage;
 }
 
 #pragma mark - UIImagePickerDelegate
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
-    // Dismiss the imagepicker
-    if (self.imagePickerPopoverController) {
-        [self.imagePickerPopoverController dismissPopoverAnimated:YES];
-        self.imagePickerPopoverController = nil;
-    }
-    else {
-        [[picker presentingViewController] dismissViewControllerAnimated:YES completion:nil];
-    }
+  [[picker presentingViewController] dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
-    // Dismiss the imagepicker
-    if (self.imagePickerPopoverController) {
-        [self.imagePickerPopoverController dismissPopoverAnimated:YES];
-        self.imagePickerPopoverController = nil;
-    }
-    else {
-        [[picker presentingViewController] dismissViewControllerAnimated:YES completion:nil];
-    }
-    
-    [self setupWithImage:[info objectForKey:UIImagePickerControllerOriginalImage]];
-}
-
-#pragma mark - UIPopoverControllerDelegate
-
-- (void)popoverControllerDidDismissPopover:(UIPopoverController *)popoverController {
-    self.imagePickerPopoverController = nil;
+  // Dismiss the imagepicker
+  [[picker presentingViewController] dismissViewControllerAnimated:YES completion:nil];
+  [self setupWithImage:info[UIImagePickerControllerOriginalImage]];
 }
 
 @end
