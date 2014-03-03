f0318473747debd03f8964bc37912d1d8f23b842
Jack <jackwu95@gmail.com>
2014-03-02 23:02:11 -0500

    CGGraphics processing initial commit

---
+++ SpookCam-CoreGraphics/SpookCam/SpookCam/ImageProcessor.m
@@ -0,0 +1,77 @@
+//
+//  ImageProcessor.m
+//  SpookCam
+//
+//  Created by Jack Wu on 2/21/2014.
+//
+//
+
+#import "ImageProcessor.h"
+
+@interface ImageProcessor ()
+
+@end
+
+@implementation ImageProcessor
+
++ (instancetype)sharedProcessor {
+  static id sharedInstance = nil;
+  
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    sharedInstance = [[self alloc] init];
+  });
+  
+  return sharedInstance;
+}
+
+#pragma mark - Public
+
+- (void)processImage:(UIImage*)inputImage {
+  UIImage * outputImage = [self processUsingCoreGraphics:inputImage];
+  
+  if ([self.delegate respondsToSelector:
+       @selector(imageProcessorFinishedProcessingWithImage:)]) {
+    [self.delegate imageProcessorFinishedProcessingWithImage:outputImage];
+  }
+}
+
+#pragma mark - Private
+
+- (UIImage *)processUsingCoreGraphics:(UIImage*)input {
+  
+  CGRect imageRect = {CGPointZero,input.size};
+  NSInteger width = CGRectGetWidth(imageRect);
+  NSInteger height = CGRectGetHeight(imageRect);
+  
+  // Grayscale color space
+  CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+  
+  // Create bitmap content with current image size and grayscale colorspace
+  CGContextRef context = CGBitmapContextCreate(nil, width, height,
+                           8, 0, colorSpace, (CGBitmapInfo)kCGImageAlphaNone);
+  
+  // Draw image into current context, with specified rectangle
+  // using previously defined context (with grayscale colorspace)
+  CGContextDrawImage(context, imageRect, [input CGImage]);
+  
+  // Create bitmap image info from pixel data in current context
+  CGImageRef imageRef = CGBitmapContextCreateImage(context);
+  
+  // Create a new UIImage object
+  UIImage *newImage = [UIImage imageWithCGImage:imageRef];
+  
+  // Release colorspace, context and bitmap information
+  CGColorSpaceRelease(colorSpace);
+  CGContextRelease(context);
+  CFRelease(imageRef);
+  
+  // Return the new grayscale image
+  return newImage;
+}
+
+
+#pragma mark Helpers
+
+
+@end
