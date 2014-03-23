ed97cd9eb636ba7cbaff6ea387bba93597dafaef
Jack <jackwu95@gmail.com>
2014-03-22 20:53:52 -0400

    Add more to the starter code

--- SpookCam-Starter/SpookCam/SpookCam/ImageProcessor.m
+++ SpookCam-Starter/SpookCam/SpookCam/ImageProcessor.m
@@ -38,5 +38,39 @@
 
 #pragma mark - Private
 
+#define Mask8(x) ( (x) & 0xFF )
+#define R(x) ( Mask8(x) )
+#define G(x) ( Mask8(x >> 8 ) )
+#define B(x) ( Mask8(x >> 16) )
+#define A(x) ( Mask8(x >> 24) )
+#define RGBAMake(r, g, b, a) ( Mask8(r) | Mask8(g) << 8 | Mask8(b) << 16 | Mask8(a) << 24 )
+- (UIImage *)processUsingPixels:(UIImage*)inputImage {
+  
+  // 1. Get the raw pixels of the image
+  UInt32 * inputPixels;
+  
+  CGImageRef inputCGImage = [inputImage CGImage];
+  NSUInteger inputWidth = CGImageGetWidth(inputCGImage);
+  NSUInteger inputHeight = CGImageGetHeight(inputCGImage);
+  
+  CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+  
+  NSUInteger bytesPerPixel = 4;
+  NSUInteger bitsPerComponent = 8;
+  
+  NSUInteger inputBytesPerRow = bytesPerPixel * inputWidth;
+  
+  inputPixels = (UInt32 *)calloc(inputHeight * inputWidth, sizeof(UInt32));
+  
+  CGContextRef context = CGBitmapContextCreate(inputPixels, inputWidth, inputHeight,
+                                               bitsPerComponent, inputBytesPerRow, colorSpace,
+                                               kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+  
+  CGContextDrawImage(context, CGRectMake(0, 0, inputWidth, inputHeight), inputCGImage);
+  
+ 
+  return inputImage;
+}
+
 
 @end
